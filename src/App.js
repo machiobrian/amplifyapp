9c1d1b8daa550d8f2bb8b6358e6d7748e1d052b0
machiobrian <machiobrian29@gmail.com>
2023-12-16 00:22:50 +0300

    Fix this App.js code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import React, {useState, useEffect} from "react";
-import logo from './logo.svg'
-import './App.css'
-import {API} from 'aws-amplify';
-
-// aws addition
-import '@aws-amplify/ui-react/styles.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { API } from "aws-amplify/api";
 import {
-  withAuthenticator,
+  Button,
   Flex,
+  Heading,
   Text,
   TextField,
-  Button,
-  Heading,
-  Image,
   View,
   Card,
+  Image,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
-import {listNotes} from './graphql/queries';
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+import { listNotes } from "./graphql/queries";
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from "./graphql/mutations";
+import logo from './logo.svg';
 
-const App = ({ signOut }) => {
+function App({ signOut }) {
   const [notes, setNotes] = useState([]);
-
-function App({signOut}){
-  return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo"/>
-        <Heading level={1}> We now have Authentication Service</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
 
   useEffect(() => {
     fetchNotes();
@@ -69,10 +53,58 @@ function App({signOut}){
       variables: { input: { id } },
     });
   }
+
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Authentication Service</Heading>
+      </Card>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Note Name"
+            label="Note Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Note Description"
+            label="Note Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Notes</Heading>
+      <View margin="3rem 0">
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
+      </View>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
 }
 
 export default withAuthenticator(App);
-
-// withAuthenticator component - this scaffolds out an entire user authentication
-// flow athat allows users to sign up/in reset passwords - also confirming
-// MFA
